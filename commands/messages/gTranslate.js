27ef3bcc9a067486defae704c454943a8b59f5ee
SpreeHertz <48062454+SpreeHertz@users.noreply.github.com>
2021-03-25 17:28:24 +0530

    messages category 💬

---
+++ commands/messages/gTranslate.js
@@ -0,0 +1,24 @@
+const { Client, Message, MessageEmbed, MessageReaction } = require('discord.js');
+const translate  = require('@iamtraction/google-translate')
+
+module.exports = {
+    name: "translate",
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+    const query = args.join(" ");
+    if(!query) return message.reply("Please specify a text to translate.")
+    
+    const translated = await translate(query, { to: 'en' });
+    message.channel.send(translated.text);
+
+
+
+
+
+
+
+}}
---
+++ commands/messages/say.js
@@ -0,0 +1,21 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "say",
+  category: "messages",
+  description: "Repeats what the user says after the command.",
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    const sayEmbed = new MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dyanmic: true }))
+        .setDescription(args.join(" "))
+        .setTimestamp()
+        .setColor("RANDOM")
+
+    message.channel.send(sayEmbed)
+  }
+};
